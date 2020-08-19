25383d7b53ac4c188c1d425be0e2c1a461e7e454
Rosario Holsen <rholsen@gmail.com>
2020-08-19 01:12:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  if block_given?
+    i = 0 
+    while i < array.length 
+      collection << yield array[i]
+      i += 1 
+    end
+    collection
+  else
+    false 
+  end 
+end 
 
-
+my_collect(array) {|name| name.split(" ").first 
