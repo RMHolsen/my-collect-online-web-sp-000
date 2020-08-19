a7f77d109ee56ea777623d2f653ae935f806aaf5
Rosario Holsen <rholsen@gmail.com>
2020-08-19 01:12:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
   if block_given?
     i = 0 
+    collection = []
     while i < array.length 
-      collection << yield array[i]
+      collection << yield(array[i])
       i += 1 
     end
     collection
